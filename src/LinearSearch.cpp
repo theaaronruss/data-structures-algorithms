0b074562df5c93e4c809a6b2ee6e747ac3108409
Aaron Russell <158230700+theaaronruss@users.noreply.github.com>
2025-04-10 21:09:47 -0400

    Implement linear search for array list as well

--- src/LinearSearch.cpp
+++ src/LinearSearch.cpp
@@ -12,3 +12,16 @@ bool linearSearch(Array<T> arr, T target)
     }
     return false;
 }
+
+template <typename T>
+bool linearSearch(ArrayList<T> arr, T target)
+{
+    for (int i = 0; i < arr.getSize(); i++)
+    {
+        if (arr.get(i) == target)
+        {
+            return true;
+        }
+    }
+    return false;
+}
