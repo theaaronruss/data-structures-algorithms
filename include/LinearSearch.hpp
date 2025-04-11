c672deead015c918d70a140f950b43f631ca33ee
Aaron Russell <158230700+theaaronruss@users.noreply.github.com>
2025-04-10 21:05:34 -0400

    Implement linear search

---
+++ include/LinearSearch.hpp
@@ -0,0 +1,4 @@
+#include "Array.hpp"
+
+template <typename T>
+bool linearSearch(Array<T> arr, T target);
---
+++ src/LinearSearch.cpp
@@ -0,0 +1,14 @@
+#include "../include/LinearSearch.hpp"
+
+template <typename T>
+bool linearSearch(Array<T> arr, T target)
+{
+    for (int i = 0; i < arr.getSize(); i++)
+    {
+        if (arr.get(i) == target)
+        {
+            return true;
+        }
+    }
+    return false;
+}
