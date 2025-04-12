0e0d9947e6e154ad753190300532048674477c4f
Aaron Russell <158230700+theaaronruss@users.noreply.github.com>
2025-04-11 21:52:16 -0400

    Implement linear search for linked list

--- include/LinearSearch.hpp
+++ include/LinearSearch.hpp
@@ -1,8 +1,42 @@
 #include "Array.hpp"
 #include "ArrayList.hpp"
+#include "LinkedList.hpp"
 
 template <typename T>
-bool linearSearch(Array<T> arr, T target);
+bool linearSearch(Array<T> arr, T target)
+{
+    for (int i = 0; i < arr.getSize(); i++)
+    {
+        if (arr.get(i) == target)
+        {
+            return true;
+        }
+    }
+    return false;
+}
 
 template <typename T>
-bool linearSearch(ArrayList<T> arr, T target);
+bool linearSearch(ArrayList<T> arr, T target)
+{
+    for (int i = 0; i < arr.getSize(); i++)
+    {
+        if (arr.get(i) == target)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+template <typename T>
+bool linearSearch(LinkedList<T> list, T target)
+{
+    for (int i = 0; i < list.getSize(); i++)
+    {
+        if (list.get(i) == target)
+        {
+            return true;
+        }
+    }
+    return false;
+}
